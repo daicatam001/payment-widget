41d35478f3090b022b3c2fcb8ef164a90d0282b6
tampt <tampt95@gmail.com>
2021-10-13 22:30:57 +0700

    feat: remove redundant comps, add common style

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <Header />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
+<script>
+import Header from "@/components/layout/Header.vue";
+export default {
   components: {
-    HelloWorld
-  }
-});
+    Header,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap");
+
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+
+:root {
+  --ff: "Montserrat", sans-serif;
+
+  --fs-base: 12px;
+
+  --ss-bg: #111052;
+
+  --fw-bold: 600;
+
+  --clr-light: #fff;
+  --clr-dark: #070c11;
+  --color-accent: #2af5d6;
+
+  --border-clr: #f2f7ff;
+  --bs-frame: 0px 0px 24px rgba(8, 21, 30, 0.2);
+  --bs-card: 0px 5px 12px rgba(57, 40, 71, 0.2);
+  --radius-base: 4px;
+}
+
+.app {
+  max-width: 931px;
+  margin: 0 auto;
+  box-shadow: var(--bs-frame);
 }
 </style>
