9366f83bdbf456b0dc0a2ff30785513ff6a4e0cd
tampt <tampt95@gmail.com>
2021-10-14 21:30:44 +0700

    refactor: change font link

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap");
 
 *,
 *::after,
